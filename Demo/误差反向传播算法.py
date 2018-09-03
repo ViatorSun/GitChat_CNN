ba5bbc124f2fdac762756dde57081c144a443c48
ViatorSun <32395868+ViatorSun@users.noreply.github.com>
2018-09-03 16:25:07 +0800

    Update 误差反向传播算法.py

--- Demo/误差反向传播算法.py
+++ Demo/误差反向传播算法.py
@@ -1,7 +1,7 @@
 
 #  !/usr/bin/env  python
 #  -*- coding:utf-8 -*-
-# @Time   :  2018.8
+# @Time   :  2018.
 # @Author :  绿色羽毛
 # @Email  :  lvseyumao@foxmail.com
 # @GitHub ： https://github.com/ViatorSun
