3670b027327e23bebac6c3c5d5dd8d9d19abc822
绿色羽毛 <32395868+ViatorSun@users.noreply.github.com>
2018-09-06 09:49:18 +0800

    Create regression.py

---
+++ Demo/Linear Regression/regression.py
@@ -0,0 +1,42 @@
+#  !/usr/bin/env  python
+#  -*- coding:utf-8 -*-
+# @Time   :  2018.
+# @Author :  绿色羽毛
+# @Email  :  lvseyumao@foxmail.com
+# @Blog   :  https://blog.csdn.net/ViatorSun
+# @Note   :  
+
+
+from numpy import *
+
+# 加载数据集，返回数据点与标签
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t')) - 1  # 获取数据样本的个数
+    dataMat = [];
+    labelMat = []
+    fr = open(fileName)
+
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+
+# 计算回归函数
+def standRegres(xArr, yArr):
+    xMat = mat(xArr);
+    yMat = mat(yArr).T
+    xTx = xMat.T * xMat
+
+    # 计算矩阵的行列式，如果行列式为0，输出错误提示
+    if linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = xTx.I * (xMat.T * yMat)
+    return ws
+
+
