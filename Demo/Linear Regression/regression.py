6a0576f04af2a5d42f6ea26c12a18179d06a53be
绿色羽毛 <32395868+ViatorSun@users.noreply.github.com>
2018-09-06 10:40:36 +0800

    Update regression.py

--- Demo/Linear Regression/regression.py
+++ Demo/Linear Regression/regression.py
@@ -11,7 +11,7 @@ from numpy import *
 
 # 加载数据集，返回数据点与标签
 def loadDataSet(fileName):
-    numFeat = len(open(fileName).readline().split('\t')) - 1  
+    numFeat = len(open(fileName).readline().split('\t')) - 1  # 获取数据样本的个数
     dataMat = [];
     labelMat = []
     fr = open(fileName)
@@ -36,7 +36,41 @@ def standRegres(xArr, yArr):
     if linalg.det(xTx) == 0.0:
         print("This matrix is singular, cannot do inverse")
         return
+
+    # 求解权重核心代码
     ws = xTx.I * (xMat.T * yMat)
     return ws
+
+
+# 局部加权线性回归
+# testPoint为输入，参数k控制衰减速度; 函数返回加权线性回归的预测值
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = mat(xArr);
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]               # 创建对角矩阵
+    weights = mat(eye((m)))
+    for j in range(m):               # 创建权重核，其中权重值大小以指数级衰减
+        diffMat = testPoint - xMat[j, :]  #
+        weights[j, j] = exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+
+    # 求解权重核心代码
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+# 用于为数据集中每个点调用lwlr()，有助于求解参数 k 的大小
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
 
 
